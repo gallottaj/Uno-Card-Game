36afc0643551eb21f534400235644f384706231c
Joe Gallotta <joeygallotta@gmail.com>
2020-01-06 16:31:50 -0800

    create card getters and setters

--- UnoCardGame/src/UnoCard.java
+++ UnoCardGame/src/UnoCard.java
@@ -9,14 +9,39 @@ public class UnoCard {
 			return Color.colors[i];
 		}
 	}
-}
 
-enum Value {
-	Zero, One, Two, Three, Four, Five, Six, Seven, Eight, Nine, Wild, Reverse, DrawTwo, DrawFour;
+	enum Value {
+		Zero, One, Two, Three, Four, Five, Six, Seven, Eight, Nine, Wild, Reverse, DrawTwo, DrawFour;
 
-	private static final Value[] values = Value.values();
+		private static final Value[] values = Value.values();
 
-	public static Value getValue(int i) {
-		return Value.values[i];
+		public static Value getValue(int i) {
+			return Value.values[i];
+		}
+	}
+
+// declare private and final so it can't be accessed by another class and can't be changed
+
+	private static Color color;
+	private static Value value;
+
+	public void UnoCard(final Color color, final Value value) {
+		this.color = color;
+		this.value = value;
+	}
+
+	public Color getColor() {
+		return this.color;
+
+	}
+
+	public Value getValue() {
+		return this.value;
+
+	}
+
+// for testing 
+	public String toString() {
+		return color + " " + value;
 	}
 }
